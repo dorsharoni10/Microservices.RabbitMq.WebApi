ad142d7cd5a0a223be713dfd4f62e7f3868bcc0f
Sharoni, Dor <ds185465@ncr.com>
2023-01-01 14:26:16 +0200

    publish Models and Pablishe

---
+++ Models/Tiket.cs
@@ -0,0 +1,11 @@
+﻿
+using System;
+
+namespace Models;
+public class Tiket
+{
+        public string UserName { get; set; }
+        public DateTime BookedOn { get; set; }
+        public string Boarding { get; set; }
+        public string Destination { get; set; }    
+}
